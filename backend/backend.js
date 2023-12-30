3b08be985ed8e992b1bdd49685c4024180d3c3f6
Roosa Pasanen <Roosa Pasanen>
2023-12-30 11:58:55 +0200

    backend.js: Set up router

--- backend/backend.js
+++ backend/backend.js
@@ -1,7 +1,7 @@
 const express = require("express"); // Import the express module
 require("dotenv").config();
 const app = express(); // Create an instance of an express application
-const locationsRouter = express.Router();
+const router = express.Router();
 const port = 8080; // Define the port on which the server will run
 const mysql = require("mysql");
 const connection = mysql.createConnection({
@@ -11,3 +11,4 @@ const connection = mysql.createConnection({
   database: process.env.DATABASE,
   multipleStatements: true,
 });
+router.use(express.json());
