91d5a7e1a4f6ffaa65a9d9c73c9cb8f579f61b28
Roosa Pasanen <Roosa Pasanen>
2023-12-30 13:10:27 +0200

    backend.js: Add connection to mysql server

--- backend/backend.js
+++ backend/backend.js
@@ -12,3 +12,23 @@ const connection = mysql.createConnection({
   multipleStatements: true,
 });
 router.use(express.json());
+
+let server = undefined;
+connection.connect((err) => {
+  // mysql connection
+  if (err) {
+    console.error("Error connecting to MySQL:", err);
+    process.exit(1);
+  } else {
+    console.log("MySQL connection successful.");
+    server = app
+      .listen(port, () => {
+        console.log(`Server listening on port ${port}`);
+        console.log(process.env);
+      })
+      .on("error", (err) => {
+        console.error("Error starting server:", err);
+        process.exit(1);
+      });
+  }
+});
