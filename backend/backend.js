a5ece5b732b20f41f4a65848ef1649483fbe3c56
Roosa Pasanen <Roosa Pasanen>
2023-12-30 11:52:26 +0200

    backend.js: Import modules
    Modules imported:
        - Express
        - Dotenv
        - Mysql

---
+++ backend/backend.js
@@ -0,0 +1,13 @@
+const express = require("express"); // Import the express module
+require("dotenv").config();
+const app = express(); // Create an instance of an express application
+const locationsRouter = express.Router();
+const port = 8080; // Define the port on which the server will run
+const mysql = require("mysql");
+const connection = mysql.createConnection({
+  host: process.env.HOST,
+  user: process.env.USER,
+  password: process.env.PASSWORD,
+  database: process.env.DATABASE,
+  multipleStatements: true,
+});
