8f5d979edbfce49f911be264f749ed6d6af1deb0
Roosa Pasanen <Roosa Pasanen>
2024-01-02 12:37:07 +0200

    wordBank.js: Add default route
    Used for any request.
    Currently used for testing the database connection.

--- backend/routes/wordBank.js
+++ backend/routes/wordBank.js
@@ -3,4 +3,18 @@ const router = express.Router();
 const connect = require("../connect.js");
 router.use(express.json());
 
+router.get("/", async (req, res) => {
+  try {
+    const quer = `SELECT w1.name, w2.name
+        FROM word as w1 INNER JOIN wordPair INNER JOIN word as w2
+        WHERE pairId1 = w1.id AND pairId2 = w2.id
+        AND langPairId = ${1};`;
+    //get data
+    const wordbank = await connect.findData(quer);
+    console.log(wordbank);
+  } catch (err) {
+    res.json(err);
+  }
+});
+
 module.exports = router;
