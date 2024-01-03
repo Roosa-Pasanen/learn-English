fbe6af5fd2d3866dcdef02248067d3e4c888792f
Roosa Pasanen <Roosa Pasanen>
2024-01-03 13:30:01 +0200

    WordTest.jsx: Fetch and display database data

--- frontend/src/WordTest.jsx
+++ frontend/src/WordTest.jsx
@@ -1,3 +1,15 @@
-function WordTest() {}
+let b = "b";
 
-module.exports = WordTest;
+export default function WordTest() {
+  const fetchInfo = async () => {
+    const data = await fetch(`${import.meta.env.VITE_API_URL}/api/wordbank`);
+    b = JSON.stringify(data);
+  };
+
+  return (
+    <div>
+      <button onClick={() => fetchInfo()}>Fetch</button>
+      <div>{b}</div>
+    </div>
+  );
+}
