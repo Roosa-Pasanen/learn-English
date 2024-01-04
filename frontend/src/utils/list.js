eedcbb8288394e5be02e810dcdf15a48a3af2691
Roosa Pasanen <Roosa Pasanen>
2024-01-04 13:27:29 +0200

    list.js:(plainList): Add JSDoc

--- frontend/src/utils/list.js
+++ frontend/src/utils/list.js
@@ -1,3 +1,13 @@
+/**
+ * A function for parsing object lists into text lists
+ *
+ * @param {object} l - list of objects
+ * @param {string} end1 - object key 1
+ * @param {string} end2 - object key 2
+ * @param {string} s - character or string that separates the keys
+ * @returns - Parsed list
+ */
+
 const plainList = (l, end1, end2, s) => {
   let fullList = [];
   for (let i = 0; i < l.length; i++) {
