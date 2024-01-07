cb153b528018cb1a738b1d0ff841c0b36c3e46de
Roosa Pasanen <Roosa Pasanen>
2024-01-07 13:41:24 +0200

    PrepTest.jsx: Add base logic
    PrepTest.jsx fetches and returns a list of words from the database.

--- frontend/src/PrepTest.jsx
+++ frontend/src/PrepTest.jsx
@@ -1,4 +1,15 @@
 import connect from "utils/connector.js";
 import plainList from "utils/list.js";
 
-export default function PrepTest() {}
+export default function PrepTest() {
+  const list = async () => {
+    const info = await connect.fetchInfo();
+    if (info == undefined) {
+      return <div>loading</div>;
+    } else {
+      return plainList(info, "fin", "eng", "-");
+    }
+  };
+
+  return list();
+}
