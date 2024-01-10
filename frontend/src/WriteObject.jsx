6fc4fa7428447e1016dcffe0c862d92b3e182a55
Roosa Pasanen <Roosa Pasanen>
2024-01-10 12:05:06 +0200

    WriteObject.jsx: Add logic
    Object receives a question which needs to be answered correctly
    Added:
        - States for variables
        - A function for returning the question
        - A function for checking the answer
        - A function for confirming which state should be returned

--- frontend/src/WriteObject.jsx
+++ frontend/src/WriteObject.jsx
@@ -1,1 +1,47 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
+
+export default function WriteObject(props) {
+  const [prompt] = useState(props.lang1);
+  const [cAnswer] = useState(props.lang2);
+  const [answer, setAnswer] = useState("");
+  const [check] = useState(props.check);
+
+  const test = () => {
+    return (
+      <div>
+        <div>{prompt}</div>
+        <input type="text" onChange={(e) => setAnswer(e.target.value)}></input>
+      </div>
+    );
+  };
+
+  const grading = () => {
+    if (answer == cAnswer) {
+      return (
+        <div>
+          <div>{prompt}</div>
+          <div> {answer} </div>
+          <p>Correct!</p>
+        </div>
+      );
+    } else {
+      return (
+        <div>
+          <div>{prompt}</div>
+          <div> {answer} </div>
+          <p>Correct answer: {cAnswer}</p>
+        </div>
+      );
+    }
+  };
+
+  const toCheck = () => {
+    if (!check) {
+      return test();
+    } else {
+      return grading();
+    }
+  };
+
+  return toCheck();
+}
