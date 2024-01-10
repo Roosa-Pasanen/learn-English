7fddee859a46f6703386ba61dc60521b39ad9201
Roosa Pasanen <Roosa Pasanen>
2024-01-10 12:26:39 +0200

    WriteTest.jsx: Add WriteObject list function

--- frontend/src/WriteTest.jsx
+++ frontend/src/WriteTest.jsx
@@ -5,4 +5,18 @@ import { WriteObject } from "./WriteObject.jsx";
 export default function WriteTest() {
   const { promptList } = useLocation();
   const [checkState, setCheckState] = useState(false);
+
+  const createList = () => {
+    let questionList = [];
+    for (let i = 0; i < promptList.length; i++) {
+      const temp = (
+        <WriteObject
+          lang1={promptList[i].lang1}
+          lang2={promptList[i].lang2}
+          check={checkState}
+        />
+      );
+      questionList.push(temp);
+    }
+  };
 }
