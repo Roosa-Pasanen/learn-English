adcf5bd4c7b32fabfb8e94e17ab22fe354cecd2d
Roosa Pasanen <Roosa Pasanen>
2024-01-10 12:22:37 +0200

    WriteTest.jsx: Add variables

--- frontend/src/WriteTest.jsx
+++ frontend/src/WriteTest.jsx
@@ -1,2 +1,8 @@
 import { useLocation } from "react-router-dom";
 import { useState } from "react";
+import { WriteObject } from "./WriteObject.jsx";
+
+export default function WriteTest() {
+  const { promptList } = useLocation();
+  const [checkState, setCheckState] = useState(false);
+}
