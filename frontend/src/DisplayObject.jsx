674bdb03ae561274fd8c15d89a4957b4f2dea909
Roosa Pasanen <Roosa Pasanen>
2024-01-12 12:14:00 +0200

    DisplayObject.jsx: Add base logic
    Add support for changing between editing and non-editing states
    Add the editing state
    Add the non-editing state

---
+++ frontend/src/DisplayObject.jsx
@@ -0,0 +1,71 @@
+import { useState, useEffect } from "react";
+
+export default function WriteObject(props) {
+  const [prompt] = useState(props.lang1);
+  const [answer] = useState(props.lang2);
+  const [editable, setEditable] = useState(false);
+  const [newPrompt, setNewPrompt] = useState(props.lang1);
+  const [newAnswer, setNewAnswer] = useState(props.lang2);
+
+  useEffect(() => {}, [props.lang1, props.lang2]);
+
+  const editing = () => {
+    return (
+      <div>
+        <input
+          type="text"
+          value={newPrompt}
+          onChange={(e) => setNewPrompt(e.target.value)}
+        ></input>
+        <input
+          type="text"
+          value={newAnswer}
+          onChange={(e) => setNewAnswer(e.target.value)}
+        ></input>
+        <div>
+          <button
+            onClick={() => {
+              setEditable(false);
+            }}
+          >
+            Cancel
+          </button>
+          <button
+            onClick={() => {
+              setEditable(false);
+            }}
+          >
+            Save
+          </button>
+        </div>
+      </div>
+    );
+  };
+
+  const display = () => {
+    return (
+      <div>
+        <div>{prompt}</div>
+        <div>{answer}</div>
+        <button
+          onClick={() => {
+            setEditable(true);
+          }}
+        >
+          Edit
+        </button>
+      </div>
+    );
+  };
+
+  const isEditable = () => {
+    console.log(editable);
+    if (!editable) {
+      return display();
+    } else {
+      return editing();
+    }
+  };
+
+  return isEditable();
+}
