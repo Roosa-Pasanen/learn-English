decc5661a225af7b792f33b763770d1d8992855b
Roosa Pasanen <Roosa Pasanen>
2024-01-12 13:51:49 +0200

    wordBank.js: Modify sql query for more information
    Previous information
        - Word1 name
        - Word2 name
    
    New additional information
        - Word1 ID
        - Word1 language ID
        - Word1 language name
        - Word2 ID
        - Word2 language ID
        - Word2 language name

--- backend/routes/wordBank.js
+++ backend/routes/wordBank.js
@@ -5,9 +5,15 @@ router.use(express.json());
 
 router.get("/", async (req, res) => {
   try {
-    const quer = `SELECT w1.name as lang1, w2.name as lang2
-        FROM word as w1 INNER JOIN wordPair INNER JOIN word as w2
+    const quer = `SELECT w1.name as word1, w1.id as wordId1,
+        w2.name as word2, w2.id as wordId2,
+        l1.id as langId1, l1.name as lang1,
+        l2.id as langId2, l2.name as lang2
+        FROM language as l1 INNER JOIN word as w1
+        INNER JOIN wordPair
+        INNER JOIN word as w2 INNER JOIN language as l2
         WHERE pairId1 = w1.id AND pairId2 = w2.id
+        AND l1.id = w1.langId AND l2.id = w2langId
         AND langPairId = ${1};`;
     //get data
     const wordbank = await connect.findData(quer);
