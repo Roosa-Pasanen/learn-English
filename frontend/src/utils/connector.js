05f79709419e14b6c656f4ab96acea452c10c5a4
Roosa Pasanen <Roosa Pasanen>
2024-01-12 14:12:03 +0200

    connector.js: Add put method for updating entries

--- frontend/src/utils/connector.js
+++ frontend/src/utils/connector.js
@@ -9,6 +9,19 @@ const connector = {
         callback(err);
       });
   },
+  putEntry: (callback, wordId, id, name, langId) => {
+    fetch(`${import.meta.env.VITE_API_URL}/api/wordbank`, {
+      method: "put",
+      headers: {
+        "Content-type": "application/json; charset=UTF-8",
+      },
+      body: JSON.stringify({
+        id,
+        name,
+        langId,
+      }),
+    });
+  },
 };
 
 export default connector;
