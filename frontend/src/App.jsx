afd88a59edb7e0828a03d77eca8075f3f5ed241c
Roosa Pasanen <Roosa Pasanen>
2024-01-15 16:09:59 +0200

    App.jsx: Add GlobalContext variables

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,26 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { useState } from "react";
 import "./App.css";
 import PrepTest from "./PrepTest";
 import WriteTest from "./WriteTest";
 import GlobalContext from "./GlobalContext.jsx";
 
 function App() {
+  const [adminState, setAdminState] = useState(false);
+  const admin = { adminState };
+
   return (
     <>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={"wip"} />
-          <Route path="/test" element={<PrepTest />} />
-          <Route path="/test/begin" element={<WriteTest />} />
-        </Routes>
-      </BrowserRouter>
+      <button onClick={() => setAdminState(true)}> Admin </button>
+      <GlobalContext.Provider value={admin}>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={"wip"} />
+            <Route path="/test" element={<PrepTest />} />
+            <Route path="/test/begin" element={<WriteTest />} />
+          </Routes>
+        </BrowserRouter>
+      </GlobalContext.Provider>
     </>
   );
 }
