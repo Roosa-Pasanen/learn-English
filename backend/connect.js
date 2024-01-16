f474c5e7f26186b0004a3b47aaf4a104cf14120b
Roosa Pasanen <Roosa Pasanen>
2024-01-16 19:01:20 +0200

    move SQL query forming connect.js -> wordBank.ks

--- backend/connect.js
+++ backend/connect.js
@@ -8,6 +8,19 @@ const connection = mysql.createConnection({
   multipleStatements: true,
 });
 
+const contact = (q) => {
+  return new Promise((resolve, reject) => {
+    connection.query(q, (err, result) => {
+      // Error handling
+      if (err) {
+        reject({ err });
+      }
+      //Return all data
+      resolve(result);
+    });
+  });
+};
+
 const databaseFunctions = {
   connect: (callback) => {
     connection.connect((err) => {
@@ -22,16 +35,86 @@ const databaseFunctions = {
       err ? callback(err) : callback();
     });
   },
-  contact: (q) => {
+  selectAll: (langid) => {
     return new Promise((resolve, reject) => {
+      const q = `SELECT w1.name as word1, w1.id as wordId1,
+        w2.name as word2, w2.id as wordId2,
+        l1.id as langId1, l1.name as lang1,
+        l2.id as langId2, l2.name as lang2
+        FROM language as l1 INNER JOIN word as w1
+        INNER JOIN wordPair
+        INNER JOIN word as w2 INNER JOIN language as l2
+        WHERE pairId1 = w1.id AND pairId2 = w2.id
+        AND l1.id = w1.langId AND l2.id = w2.langId
+        AND langPairId = ${connection.escape(langid)};`;
       connection.query(q, (err, result) => {
-        // Error handling
         if (err) {
-          reject({ err });
+          reject(err);
         }
-        //Return all data
         resolve(result);
       });
+    });
+  },
+  updateWord: (id, name) => {
+    return new Promise((resolve, reject) => {
+      const q = `UPDATE word SET name = ${connection.escape(name)}
+      WHERE id = ${connection.escape(id)};`;
+      connection.query(q, (err, result) => {
+        if (err) {
+          reject(err);
+        }
+        resolve(result);
+      });
+    });
+  },
+  deleteWord: (id1, id2) => {
+    return new Promise((resolve, reject) => {
+      const q = `DELETE from wordPair WHERE pairId1 = ${connection.escape(id1)}
+      AND pairId2 = ${connection.escape(id2)};`;
+      connection.query(q, (err, result) => {
+        if (err) {
+          reject(err);
+        }
+        resolve(result);
+      });
+    });
+  },
+  postWord: (langId1, langId2, name1, name2) => {
+    return new Promise((resolve, reject) => {
+      const p = async () => {
+        const q1Search = `SELECT id from word
+          WHERE name = ${connection.escape(name1)}
+          AND langId = ${connection.escape(langId1)}
+          ORDER BY id DESC
+          LIMIT 1`;
+        const q2Search = `SELECT id from word
+          WHERE name = ${connection.escape(name2)}
+          AND langId = ${connection.escape(langId2)}
+          ORDER BY id DESC
+          LIMIT 1`;
+        const insert1 = `INSERT INTO word (name, langId)
+        VALUES (${connection.escape(name1)}, ${connection.escape(langId1)});`;
+        const insert2 = `INSERT INTO word (name, langId)
+          VALUES (${connection.escape(name2)}, ${connection.escape(langId2)});`;
+
+        contact(insert1)
+          .then(() => contact(insert2).catch((err) => console.log(err)))
+          .then(() => {
+            const insert3 = `INSERT INTO wordPair (pairId1, pairId2, langPairId)
+            VALUES ((${q1Search}), (${q2Search}),
+            (SELECT id FROM languagePair
+            WHERE pairID1 = ${connection.escape(langId1)}
+            AND pairID2 = ${connection.escape(langId2)}));`;
+            contact(insert3).catch((err) => console.log(err));
+          })
+          .catch((err) => console.log(err));
+      };
+      try {
+        p();
+        resolve();
+      } catch (err) {
+        reject(err);
+      }
     });
   },
 };
--- backend/routes/wordBank.js
+++ backend/routes/wordBank.js
@@ -5,19 +5,20 @@ router.use(express.json());
 
 router.get("/", async (req, res) => {
   try {
-    const quer = `SELECT w1.name as word1, w1.id as wordId1,
-        w2.name as word2, w2.id as wordId2,
-        l1.id as langId1, l1.name as lang1,
-        l2.id as langId2, l2.name as lang2
-        FROM language as l1 INNER JOIN word as w1
-        INNER JOIN wordPair
-        INNER JOIN word as w2 INNER JOIN language as l2
-        WHERE pairId1 = w1.id AND pairId2 = w2.id
-        AND l1.id = w1.langId AND l2.id = w2.langId
-        AND langPairId = ${1};`;
-    //get data
-    const wordbank = await connect.contact(quer);
-    res.send(wordbank);
+    connect
+      .selectAll(1)
+      .then((data) => {
+        if (data.length !== 0) {
+          res.status(200);
+          res.send(data);
+        } else {
+          res.status(404);
+        }
+      })
+      .catch((err) => {
+        res.status(500);
+        res.json(err);
+      });
   } catch (err) {
     res.json(err);
   }
@@ -25,25 +26,32 @@ router.get("/", async (req, res) => {
 
 router.put("/word/:id([0-9]+)", async (req, res) => {
   try {
-    const q = `UPDATE word
-  SET name = "${req.body.name}"
-  WHERE id = ${req.params.id};`;
-    await connect.contact(q);
-    res.send("Success!");
+    connect
+      .updateWord(req.params.id, req.body.name)
+      .then(() => {
+        res.status(204);
+      })
+      .catch((err) => {
+        res.status(500);
+        res.json(err);
+      });
   } catch (error) {
-    console.log(error);
     res.json(error);
   }
 });
 
 router.delete("/word", async (req, res) => {
   try {
-    const q = `DELETE from wordPair WHERE pairId1 = ${req.body.id1} AND
-    pairId2 = ${req.body.id2};`;
-    await connect.contact(q);
-    res.send("Success!");
+    connect
+      .deleteWord(req.body.id1, req.body.id2)
+      .then(() => {
+        res.status(204);
+      })
+      .catch((err) => {
+        res.status(500);
+        res.json(err);
+      });
   } catch (error) {
-    console.log(error);
     res.json(error);
   }
 });
@@ -51,30 +59,22 @@ router.delete("/word", async (req, res) => {
 router.post("/word", async (req, res) => {
   try {
     // Name, languageid
-    const q1Search = `SELECT id from word
-    WHERE name = "${req.body.name1}" AND langId = ${req.body.langId1};`;
-    const q2Search = `SELECT id from word
-    WHERE name = "${req.body.name2}" AND langId = ${req.body.langId2};`;
-    let id1 = await connect.contact(q1Search);
-    let id2 = await connect.contact(q2Search);
-    if (id1[0] == undefined) {
-      const insert = `INSERT INTO word (name, langId)
-      VALUES ("${req.body.name1}", ${req.body.langId1});`;
-      await connect.contact(insert);
-      id1 = await connect.contact(q1Search);
-    }
-    if (id2[0] == undefined) {
-      const insert = `INSERT INTO word (name, langId)
-      VALUES ("${req.body.name2}", ${req.body.langId2});`;
-      await connect.contact(insert);
-      id2 = await connect.contact(q2Search);
-    }
-    const q3 = `INSERT INTO wordPair (pairId1, pairId2, langPairId)
-      VALUES (${id1[0].id}, ${id2[0].id}, (SELECT id FROM languagePair
-      WHERE pairID1 = ${req.body.langId1} AND pairID2 = ${req.body.langId2}));`;
-    await connect.contact(q3);
-
-    res.send("Success!");
+    connect
+      .postWord(
+        req.body.langId1,
+        req.body.langId2,
+        req.body.name1,
+        req.body.name2
+      )
+      .then(() => {
+        console.log("läpi");
+        res.status(201);
+      })
+      .catch((err) => {
+        res.status(500);
+        res.json(err);
+        console.log(err);
+      });
   } catch (error) {
     console.log(error);
     res.json(error);
