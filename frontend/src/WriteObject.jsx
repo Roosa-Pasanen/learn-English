0b8961b587afd06e64924303fc25bd250b7ee069
Roosa Pasanen <Roosa Pasanen>
2024-01-17 17:51:31 +0200

    WriteObject.jsx: Add bootstrap styling

--- frontend/src/WriteObject.jsx
+++ frontend/src/WriteObject.jsx
@@ -1,4 +1,7 @@
 import { useState, useEffect } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+import { Card } from "react-bootstrap";
 
 export default function WriteObject(props) {
   const [prompt] = useState(props.word1);
@@ -10,29 +13,42 @@ export default function WriteObject(props) {
 
   const test = () => {
     return (
-      <div>
-        <div>{prompt}</div>
-        <input type="text" onChange={(e) => setAnswer(e.target.value)}></input>
-      </div>
+      <Card className="m-2">
+        <Card.Body className="d-flex justify-content-between align-items-center">
+          <div className="mx-3">{prompt}</div>
+          <input
+            type="text"
+            onChange={(e) => setAnswer(e.target.value)}
+          ></input>
+        </Card.Body>
+      </Card>
     );
   };
 
   const grading = () => {
     if (answer == cAnswer) {
       return (
-        <div>
-          <div>{prompt}</div>
-          <div> {answer} </div>
-          <p>Correct!</p>
-        </div>
+        <Card className="m-2">
+          <Card.Body>
+            <div className="d-flex justify-content-around align-items-center">
+              <div>{prompt}</div>
+              <div> {answer} </div>
+              <p>Correct!</p>
+            </div>
+          </Card.Body>
+        </Card>
       );
     } else {
       return (
-        <div>
-          <div>{prompt}</div>
-          <div> {answer} </div>
-          <p>Correct answer: {cAnswer}</p>
-        </div>
+        <Card className="m-2">
+          <Card.Body className="d-flex justify-content-around align-items-center">
+            <div className="d-flex justify-content-around align-items-center">
+              <div>{prompt}</div>
+              <div> {answer} </div>
+              <p>Correct answer: {cAnswer}</p>
+            </div>
+          </Card.Body>
+        </Card>
       );
     }
   };
