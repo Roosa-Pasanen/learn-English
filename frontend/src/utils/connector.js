11be3a759fb764a9b08be3682ea24ae9188a3358
Roosa Pasanen <Roosa Pasanen>
2024-01-19 14:48:26 +0200

    connector.js: Add jsdoc

--- frontend/src/utils/connector.js
+++ frontend/src/utils/connector.js
@@ -1,4 +1,14 @@
+/**
+ * Connector object for making requests to backend
+ */
 const connector = {
+  /**
+   * Function for sending fetch requests
+   *
+   * Retreives information from the database and returns it in a callback
+   *
+   * @param {function} callback Callback function
+   */
   fetchInfo: (callback) => {
     fetch(`${import.meta.env.VITE_API_URL}/api/wordbank`)
       .then((res) => res.json())
@@ -9,8 +19,19 @@ const connector = {
         callback(err);
       });
   },
-  putEntry: (callback, id, name, type) => {
-    fetch(`${import.meta.env.VITE_API_URL}/api/wordbank/${type}/${id}`, {
+  /**
+   * Function for sending put requests
+   *
+   * Used for updating existing information in the database
+   * Returns response status in callback
+   *
+   * @param {function} callback - Callback function
+   * @param {number} id - ID of the item in need of updating
+   * @param {string} name - (New) name of the item in need of updating
+   * @param {string} add - Address extention
+   */
+  putEntry: (callback, id, name, add) => {
+    fetch(`${import.meta.env.VITE_API_URL}/api/wordbank/${add}/${id}`, {
       method: "put",
       headers: {
         "Content-type": "application/json; charset=UTF-8",
@@ -28,8 +49,19 @@ const connector = {
         callback(err);
       });
   },
-  deleteEntry: (callback, id1, id2, type) => {
-    fetch(`${import.meta.env.VITE_API_URL}/api/wordbank/${type}`, {
+  /**
+   * Function for sending pair delete requests
+   *
+   * Used for severing the link between two items in the database
+   * Returns response status in callback
+   *
+   * @param {function} callback - Callback function
+   * @param {number} id1 - ID of the first item to be deleted
+   * @param {number} id2 - ID of the second item to be deleted
+   * @param {string} add - Address extention
+   */
+  deleteEntry: (callback, id1, id2, add) => {
+    fetch(`${import.meta.env.VITE_API_URL}/api/wordbank/${add}`, {
       method: "delete",
       headers: {
         "Content-type": "application/json; charset=UTF-8",
@@ -47,6 +79,18 @@ const connector = {
         callback(err);
       });
   },
+  /**
+   * Function for posting new "word" items to the database
+   *
+   * Used for posting new items and creating a link between them
+   * Returns the response status in a callback
+   *
+   * @param {function} callback - Callback function
+   * @param {string} name1 - Name of the first new object
+   * @param {number} langId1 - LanguageID of the first new object
+   * @param {string} name2 - Name of the second new object
+   * @param {number} langId2 - LanguageID of the second new object
+   */
   postWord: (callback, name1, langId1, name2, langId2) => {
     fetch(`${import.meta.env.VITE_API_URL}/api/wordbank/word`, {
       method: "POST",
